8218ed6c3e3af4497a30f398ccc9b9978bd53e2c
Simon Gravelle <simon.gravelle@live.fr>
2025-11-27 23:27:33 +0100

    Added CO2

---
+++ BulkCO2/GCMC-chemical-potential-LAMMPS/run-chemical-potential.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+lmp=/home/simon/Softwares/lammps-10Sep2025/src/lmp_mpi
+
+# List of pressures
+mus=(-8.6 -8.4 -8.2 -8.0 -7.8 -7.6)
+
+# Path to your reference input file
+ref_input="reference-files/input.lmp"
+
+# Number of parallel jobs
+max_jobs=6   # change this depending on CPU/MPI resources
+job_count=0
+
+# Loop over pressures
+for mu in "${mus[@]}"; do
+    folder="mu_${mu}kcalmol"
+    echo "Creating folder: $folder"
+
+    # Make directory
+    mkdir -p "$folder"
+
+    # Copy the input file
+    cp "$ref_input" "$folder/"
+
+    # Modify the pressure line
+    # Changes: variable pressure equal X
+    sed -i "s/^variable mu equal .*/variable mu equal ${mu}/" "$folder/input.lmp"
+
+    cd "$folder"
+    mpirun -np 4 ${lmp} -in input.lmp
+    cd ..
+
+    # # Run LAMMPS in the background, redirect output to log
+    # (
+    #     cd "$folder"
+    #     echo "Running LAMMPS for mu=${mu} ..."
+    #     mpirun -np 4 ${lmp} -in input.lmp > lammps.log 2>&1
+    # ) &
+
+    # ((job_count++))
+    # # Limit the number of parallel jobs
+    # if (( job_count % max_jobs == 0 )); then
+    #     wait
+    # fi
+done
---
+++ BulkCO2/GCMC-chemical-potential-MANIAC/run-chemical-potential.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+lmp=/home/simon/Softwares/lammps-22Jul2025/src/lmp_mpi
+
+# List of pressures
+mus=(-8.6 -8.4 -8.2 -8.0 -7.8 -7.6)
+
+# Path to your reference input file
+ref_input="reference-files/input.maniac"
+ref_bash="reference-files/run.sh"
+
+# Loop over pressures
+for mu in "${mus[@]}"; do
+    folder="mu_${mu}kcalmol"
+    echo "Creating folder: $folder"
+
+    # Make directory
+    mkdir -p "$folder"
+
+    # Copy the input file
+    cp "$ref_input" "$folder/"
+    cp "$ref_bash" "$folder/"
+
+    sed -i "s/^[[:space:]]*chemical_potential .*/  chemical_potential ${mu}/" "$folder/input.maniac"
+
+    # Move into folder and run Maniac in background
+    (cd "$folder" && ./run.sh) &
+done
