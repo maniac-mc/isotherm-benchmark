0571a17119a6b74d15fcdbdf67fb9e945c176802
Simon Gravelle <simon.gravelle@live.fr>
2025-11-28 09:17:24 +0100

    Added comparison CO2

--- BulkCO2/GCMC-chemical-potential-LAMMPS/extract_isotherm.py
+++ BulkCO2/GCMC-chemical-potential-LAMMPS/extract_isotherm.py
@@ -18,7 +18,7 @@ for folder in os.listdir(base): # Loop through folders
         mu_flt = np.float32(mu_str)
 
         # Read data file
-        nb_path = os.path.join(base, folder, "outputs/number_co2.dat")
+        nb_path = os.path.join(base, folder, "nb.dat")
         if not os.path.exists(nb_path):
             continue
 
