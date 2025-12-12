81f457830046f16a4be5314c192a3dd091ae3c79
Simon Gravelle <simon.gravelle@live.fr>
2025-12-12 17:51:42 +0100

    Updated results

--- BulkCO2/GCMC-MANIAC/reference-files/run.sh
+++ BulkCO2/GCMC-MANIAC/reference-files/run.sh
@@ -4,7 +4,7 @@ set -e
 input="input.maniac"
 data="topology.data"
 inc="parameters.inc"
-res="../../Reservoir/topology.data"
+# res="../../Reservoir/topology.data"
 outputs="outputs/"
 
 maniac="/home/simon/Git/Maniac/maniac-mc.github.io/build/maniac"
